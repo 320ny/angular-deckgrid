cc04c1369e36e87e1f8cc03a2ac093fa245b6003
Brian Weiner <brian@320ny.com>
2014-03-03 13:33:02 -0500

    obama

--- angular-deckgrid.js
+++ angular-deckgrid.js
@@ -283,7 +283,9 @@ angular.module('akoenig.deckgrid').factory('Deckgrid', [
         Deckgrid.prototype.$$getLayout = function $$getLayout () {
             var content = $window.getComputedStyle(this.$$elem, ':before').content,
                 layout;
-
+            if (!content){
+                content = "3 .column.column-1-3";
+            }
             if (content) {
                 content = content.replace(/'/g, '');  // before e.g. '3 .column.size-1of3'
                 content = content.replace(/"/g, '');  // before e.g. "3 .column.size-1of3"
